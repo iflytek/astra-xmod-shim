e8402034908cbffe8f11bcba37c751187bf5380b
bestksl <bestksl@163.com>
2025-09-17 16:38:52 +0800

    update llmpipeline

--- internal/pipeline/pipeline.go
+++ internal/pipeline/pipeline.go
@@ -9,10 +9,7 @@ var Registry = typereg.New[Pipeline]()
 
 // Pipeline is the interface that must be implemented by all shimlets.
 type Pipeline interface {
-	CreateDeploySpec() (spec dto.DeploySpec, err error)
-	apply(spec dto.DeploySpec) (resourceId string, err error)
-	Delete(resourceId string) error
-	Status(resourceId string) (status *dto.DeployStatus, err error)
+	Apply(spec dto.DeploySpec) (resourceId string, err error)
 	ID() (name string)
 	Description() string
 }
